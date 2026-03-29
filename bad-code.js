098cf0753a3f0efa78ea8a753178715ee11c5264
ru58841 <21xva21@gmail.com>
2026-03-29 10:48:54 +0000

    final

--- bad-code.js
+++ bad-code.js
@@ -1,9 +1,32 @@
-const DB_PASSWORD = "supersecret123";  
-
-function add(a, b) {
-    return a + c;  
+function calculateTotal(a, b, c) {
+    var result = a + b + c  
+    return result
 }
 
+function fetchData() {
+    fetch("https://api.example.com/data")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
-var x = undefined;
-console.log(x.name);  
+function login(user, pass) {
+    if (user == "admin" && pass == "1234") { 
+        return true
+    } else {
+        return false
+    }
+}
+
+function temp() {
+    let x = 10
+}
+
+function x(y) {
+    return y * 42
+}
+
+for (let i = 0; i < 1000000000; i++) {
+    console.log(i)
+}
